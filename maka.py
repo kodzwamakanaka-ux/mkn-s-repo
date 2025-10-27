6b7aa7eff33bd49ab22f484d709fc420af463da0
kodzwamakanaka-ux <kodzwamakanaka@gmail.com>
2025-10-27 07:12:21 +0200

    Update maka.py

--- maka.py
+++ maka.py
@@ -1,3 +1,11 @@
 print("hello!")
+def calculate_procedural_sum(numbers) :
+  total=0
+  for number in numbers:
+    total +=number
+    return total
+    result=calculate_procedural_sum([1,2,3,4])
+    print(f"Procedural sum : {result}")
+    
 
   
